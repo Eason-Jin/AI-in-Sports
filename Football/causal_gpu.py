d0f5b72e560c19debe96e2a8694d076be0432b7c
Eason <ejin458@aucklanduni.ac.nz>
2025-01-31 15:10:30 +1300

    aggregated links for individual player

--- Football/causal_gpu.py
+++ Football/causal_gpu.py
@@ -19,6 +19,13 @@ TAU_MAX = 5
 match_folder = 'match_0'
 df = pd.read_csv(f'Football/matches/{match_folder}/match_data_3.csv')
 
+if not os.path.exists(f'Football/matches/{match_folder}/graphs'):
+    os.makedirs(f'Football/matches/{match_folder}/graphs')
+if not os.path.exists(f'Football/matches/{match_folder}/links'):
+    os.makedirs(f'Football/matches/{match_folder}/links')
+if not os.path.exists(f'Football/matches/{match_folder}/time_series_graphs'):
+    os.makedirs(f'Football/matches/{match_folder}/time_series_graphs')
+
 all_player_results = {}
 
 print('Starting PCMCI')
@@ -28,7 +35,8 @@ for player_id in players:
     # Extract data for the current player
     player_df = searchDF(df, [('player_id', player_id)]).astype(np.float64)
     datatime = player_df['time'].to_numpy()
-    var_names = player_df.drop(columns=['player_id', 'time']).columns
+    player_df = player_df.drop(columns=['player_id', 'time'])
+    var_names = player_df.columns
 
     # Set up tigramite DataFrame
     dataframe = pp.DataFrame(
@@ -45,32 +53,45 @@ for player_id in players:
     #     val_matrix=results['val_matrix'],
     #     alpha_level=0.01)
     graph = pcmci.get_graph_from_pmatrix(p_matrix=q_matrix, alpha_level=0.01,
-                                         tau_min=0, tau_max=TAU_MAX, link_assumptions=None)
+                                         tau_min=0, tau_max=TAU_MAX)
     results['graph'] = graph
+    
+    # print("Lag-zero slice of graph:")
+    # print(graph[:, :, 0])
 
     all_player_results[player_id] = results
+
+    tp.plot_graph(
+        val_matrix=results['val_matrix'],
+        graph=results['graph'],
+        var_names=var_names,
+        link_colorbar_label='cross-MCI',
+        node_colorbar_label='auto-MCI',
+        show_autodependency_lags=False,
+        save_name=f'Football/matches/{match_folder}/graphs/graph_{player_id}.png'
+    )
+
+    tp.plot_time_series_graph(
+        figsize=(6, 4),
+        val_matrix=results['val_matrix'],
+        graph=results['graph'],
+        var_names=var_names,
+        link_colorbar_label='MCI',
+        save_name=f'Football/matches/{match_folder}/time_series_graphs/time_series_graph_{player_id}.png'
+    )
+    try:
+        tp.write_csv(
+            val_matrix=results['val_matrix'],
+            graph=results['graph'],
+            var_names=var_names,
+            save_name=f'Football/matches/{match_folder}/links/link_{player_id}.csv',
+            digits=5,
+        )
+    except ValueError:
+        print(f'No causality found for player {player_id}, writing empty csv')
+        with open(f'Football/matches/{match_folder}/links/link_{player_id}.csv', 'w') as f:
+            f.write('Variable i,Variable j,Time lag of i,Link type i --- j,Link value')
 
 with open(f'Football/matches/{match_folder}/results.pkl', 'wb') as f:
     pickle.dump(all_player_results, f)
 print('PCMCI saved')
-
-# print('Plotting graph')
-# tp.plot_graph(
-#     val_matrix=results['val_matrix'],
-#     graph=results['graph'],
-#     var_names=var_names,
-#     link_colorbar_label='cross-MCI',
-#     node_colorbar_label='auto-MCI',
-#     show_autodependency_lags=False,
-#     save_name=f'Football/matches/{match_folder}/graph.png'
-# )
-
-# print('Plotting time series graph')
-# tp.plot_time_series_graph(
-#     figsize=(6, 4),
-#     val_matrix=results['val_matrix'],
-#     graph=results['graph'],
-#     var_names=var_names,
-#     link_colorbar_label='MCI',
-#     save_name=f'Football/matches/{match_folder}/time_series_graph.png'
-# )
